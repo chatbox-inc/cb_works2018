da8a08a967210f00b8a92484c05c625f3a7a501d
takashi <b7180@oic.jp>
2018-03-12 15:08:20 +0900

    Description.vue, List.vueコーディング

--- components/Page/Detail/Description.vue
+++ components/Page/Detail/Description.vue
@@ -14,29 +14,21 @@
         <p class="p-description__text">複雑化するシステム構築の課題を、最新の制作技術でしっかりサポート多様化するニーズや、複雑化するシステム構築の課題を、最新の制作技術でしっかりサポート。Web制作に関するあらゆる相談に対応出来るよう、経験豊富なスタッフが親身に対応にあたります。</p>
       </div>
     </div>
-    <div class="p-description__list">
-      <div class="p-description__listIcon">制作</div>
-      <div class="p-description__listInner">
-        <div class="p-description__listTitle">外注を行わない一貫した制作フロー（端的に。ドメインを明示的に）</div>
-        <div class="p-description__listDrawingWrap">
-          <span class="p-description__listDrawing"></span>
-          <span class="p-description__listDrawing"></span>
-          <span class="p-description__listDrawing"></span>
-          <span class="p-description__listDrawing"></span>
-        </div>
-        <div class="p-description__listBoldText">企画から運用まで社内で行うのでコミュニケーションを円滑に安心して進捗をお任せ頂けます。(メリットを伝える)</div>
-        <p class="p-description__listText">chatboxでは制作はすべて一貫して社内で対応。だからこそ、進捗を明瞭にし、安心して頂けるコミュニケーションを心がけております。（ドメインとメリットを踏まえた上で詳細を詳しく。）</p>
-      </div>
-    </div>
+    <description-list v-for="key in 3" :key="key"></description-list>
   </section>
 </template>
 
 <script>
+import DescriptionList from "./Description/List.vue";
+
 export default {
   computed: {
     thumbStyle() {
-      return { backgroundImage: `url('/images/serviceHeading_create.jpg')` }; // http://placehold.it/350x350
+      return { backgroundImage: `url('/images/serviceHeading_create.jpg')` };
     }
+  },
+  components: {
+    DescriptionList
   }
 };
 </script>
@@ -121,67 +113,6 @@ export default {
     @include desktop {
       font-size: 16px;
       line-height: 36px;
-    }
-  }
-  &__list {
-    @include c-container;
-    color: #4d4b4d;
-    background-color: #faf7f7;
-    margin-bottom: 10px;
-    position: relative;
-    @include desktop {
-      margin-bottom: 30px;
-    }
-    &Icon {
-      position: absolute;
-      top: 0;
-      text-align: center;
-      color: #ffffff;
-      background-color: #c3504f;
-      width: 45px;
-      height: 45px;
-      line-height: 45px;
-      font-size: 14px;
-      @include desktop {
-        width: 75px;
-        height: 75px;
-        line-height: 75px;
-        font-size: 25px;
-      }
-    }
-    &Inner {
-      @include desktop {
-        margin: 0 90px;
-      }
-    }
-    &Title {
-      font-weight: bold;
-      font-size: 15px;
-      margin: 0 55px;
-      @include desktop {
-        font-size: 25px;
-        line-height: 75px;
-      }
-    }
-    &DrawingWrap {
-    }
-    &Drawing {
-    }
-    &BoldText {
-      font-weight: bold;
-      font-size: 16px;
-      line-height: 30px;
-      @include desktop {
-        font-size: 17px;
-        line-height: 24px;
-      }
-    }
-    &Text {
-      line-height: 32px;
-      @include desktop {
-        font-size: 15px;
-        line-height: 30px;
-      }
     }
   }
 }
